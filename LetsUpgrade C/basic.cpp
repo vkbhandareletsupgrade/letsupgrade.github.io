f549ad0de256ac1410460f3202014aa3afa16eed
VAIBHAV KAILAS BHANDARE <108469742+vkbhandareletsupgrade@users.noreply.github.com>
2022-07-05 20:23:42 +0530

    Update basic.cpp
    adding more functions div mult subs diretcly from github file

--- LetsUpgrade C/basic.cpp
+++ LetsUpgrade C/basic.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int a,b,c,d;
+    int a,b,c,d,e,f;
 
     cout<<endl<<"Enter a: ";
     cin>>a;
@@ -12,8 +12,11 @@ int main(){
 
     c = a+b;
     d = a*b;
+    e = a-b;
+    f = a/b;
     cout<<endl<<"Addition:"<<c;
     cout<<endl<<"Multiplication :"<<d;
-
+     cout<<endl<<"Substraction :"<<e;
+     cout<<endl<<"Division :"<<f;
     return 0;
 } 
