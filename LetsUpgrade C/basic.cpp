4af02028da5eda46e55e4ea1cb61f0fe72da6f09
vkbhandare <vkbhandare00000@users.noreply.github.com>
2022-07-05 20:20:37 +0530

    add files

---
+++ LetsUpgrade C/basic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,d;
+
+    cout<<endl<<"Enter a: ";
+    cin>>a;
+
+    cout<<endl<<"Enter b: ";
+    cin>>b;
+
+    c = a+b;
+    d = a*b;
+    cout<<endl<<"Addition:"<<c;
+    cout<<endl<<"Multiplication :"<<d;
+
+    return 0;
+} 
