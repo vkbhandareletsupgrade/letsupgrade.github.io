2bc82aedbfcaa6c475b0416364d91da99480555d
vkbhandare <vkbhandare00000@users.noreply.github.com>
2022-12-09 12:12:49 +0530

    co

---
+++ js -snake project/custom.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',()=>{
+const sqaure = document.querySelectorAll('.grid div');
+const displayScore = document.querySelector('span');
+const start = document.querySelector('.start');
+const width = 10;
+let currentiIndex = 0;
+let appleIndex = 0;
+let currentSnake = [2,1,0];
+})
